64601991562bc6d49eb7596c68ea22a655ac4e64
damilolo <adiodamilola@yahoo.com>
2018-03-17 17:41:20 -0700

    create parent index.js component

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+// index.andriod.js -- place code in here for Andriod
+
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+const App = () => {
+
+	return (
+		<Text>Hello World</Text>
+	);
+};
 
 AppRegistry.registerComponent('albums', () => App);
