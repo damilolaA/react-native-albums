eba8ed62dac6638b0840746dc6f41d31df2bef16
damilolo <adiodamilola@yahoo.com>
2018-03-19 11:24:50 -0700

    create AlbumList component and pass it to index.js parent component

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 // index.andriod.js -- place code in here for Andriod
 
 import React from 'react';
-import { AppRegistry } from 'react-native';
-import Header from './src/components/header';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 const App = () => {
 
 	return (
-		<Header headerText={'Albums!!!'} />
+		<View>
+			<Header headerText={'Albums'} />
+			<AlbumList />
+		</View>	
 	);
 };
 
