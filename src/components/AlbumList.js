48365f6511d6add0148a8439f528e97e8dfbf58e
damilolo <adiodamilola@yahoo.com>
2018-03-19 15:18:37 -0700

    define AlbumDetail component and pass it an album prop

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import Axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
+	constructor() {
+		super();
+		this.state = {
+			albums: []
+		};
+	}
 
 	componentWillMount() {
 		Axios.get('https:rallycoding.herokuapp.com/api/music_albums')
-			.then(response => console.log(response));
+			.then(response => {
+				this.setState(this.setState({ albums: response.data }));
+			});
+	}
+
+	renderAlbums() {
+		return this.state.albums.map(album => {
+			<AlbumDetail key={album.title} album={album} />;
+		});
 	}
 
 	render() {
 		return (
 			<View>
-				<Text>AlbumList</Text>
+				{this.renderAlbums()}
 			</View>
 		);
 	}
