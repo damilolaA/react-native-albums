09d1f91297403e4bf23913e4e63bf8042e387176
damilolo <adiodamilola@yahoo.com>
2018-03-21 11:52:36 -0700

    fetch and style album cover in AlbumDetail component

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -3,21 +3,36 @@ import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+
+	const { thumbnail_image, title, artist, image } = album;
+	const { 
+		thumbnailStyle, 
+		headerContentStyle, 
+		thumbnailContainerStyle, 
+		headerTextStyle,
+		imageStyle 
+	} = styles;
 
 	return (
 		<Card>
 			<CardSection>
-				<View>
+				<View style={thumbnailContainerStyle}>
 					<Image 
-						style={styles.thumbnailStyle}
-						sourve={{ uri: props.album.thumbnail_image }} 
+						style={thumbnailStyle}
+						source={{ uri: thumbnail_image }} 
 					/>
 				</View>
-				<View style={styles.headerContentStyle}>	
-					<Text>{props.album.title}</Text>
-					<Text>{props.album.artist}</Text>
+				<View style={headerContentStyle}>	
+					<Text style={headerTextStyle}>{title}</Text>
+					<Text>{artist}</Text>
 				</View>				
+			</CardSection>
+			<CardSection>
+				<Image 
+					style={imageStyle}
+					source={{ uri: image }} 
+				/>
 			</CardSection>
 		</Card>
 	);
@@ -28,9 +43,23 @@ const styles = {
 		flexDirection: 'column',
 		justifyContent: 'space-around'
 	},
+	headerTextStyle: {
+		fontSize: 18
+	},
 	thumbnailStyle: {
 		height: 50,
 		width: 50
+	},
+	thumbnailContainerStyle: {
+		marginLeft: 10,
+		marginRight: 10,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	imageStyle: {
+		height: 300,
+		flex: 1,
+		width: null
 	}
 };
 
